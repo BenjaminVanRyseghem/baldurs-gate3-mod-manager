ee53a15b9486a42f00c6f023f0f7d15a03c3205f
Benjamin Van Ryseghem <benjamin.vanryseghem@gmail.com>
2024-05-16 10:28:18 +0200

    Fix version value in createModNode
    The version is a string like "1.0.0.0" while the exported version
    must be something like "36028797018963968".

--- src/main/entities/mod/lib/helpers/createModNode/createModNode.ts
+++ src/main/entities/mod/lib/helpers/createModNode/createModNode.ts
@@ -10,6 +10,13 @@ const createModNode = (mod: ModInfo) => {
     versionType = "",
   } = mod;
 
+  const [major = 0n, minor = 0n, revision = 0n, build = 0n] = version
+    .split(".")
+    .map(BigInt);
+
+  const versionNumberString =
+    (major << 55n) + (minor << 47n) + (revision << 31n) + build; // eslint-disable-line no-bitwise
+
   const template = `
     <node id="ModuleShortDesc">
       <!--  ${name}  -->
@@ -17,7 +24,7 @@ const createModNode = (mod: ModInfo) => {
       <attribute id="MD5" type="LSString" value="${md5}" />
       <attribute id="Name" type="LSString" value="${name}" />
       <attribute id="UUID" type="FixedString" value="${uuid}" />
-      <attribute id="Version64" type="${versionType}" value="${version}" />
+      <attribute id="Version64" type="${versionType}" value="${versionNumberString.toString()}" />
     </node>
 `;
 
